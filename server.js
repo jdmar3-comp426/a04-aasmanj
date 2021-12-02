00426152c4d4e3471d9182b9f52d1b424f842319
Jordan Aasman <jjord@ad.unc.edu>
2021-12-02 17:40:15 -0500

    delete

--- server.js
+++ server.js
@@ -41,10 +41,13 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run(req.body.user,md5(req.body.pass),req.params.id);
-	res.status(201).json({"message":"1 record updated: ID %id% (200)".replace("%id", req.params.id)});
+	res.status(201).json({"message":"1 record updated: ID %id% (200)".replace("%id%", req.params.id)});
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-
+app.delete("/app/delete/user/:id", (req, res) => {
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?").run(req.params.id);
+	res.status(200).json({"message":"1 record deleted: ID %id% (200)".replace("%id%", req.params.id)});
+});
 // Default response for any other request
 app.use(function(req, res){
 	res.json({"message":"Your API is working!"});
